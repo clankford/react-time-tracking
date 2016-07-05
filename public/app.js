5fc60672d5709980981f303679dbbb0272452e1a
Chris Lankford <christopherlankford@gmail.com>
2016-07-05 15:47:04 -0700

    Added static EditableTimer

--- public/app.js
+++ public/app.js
@@ -35,3 +35,25 @@ const EditableTimerList = React.createClass({
         );
     },
 });
+
+const EditableTimer = React.createClass({
+    render: function() {
+        if (this.props.editFormOpen) {
+            return (
+                <TimerForm
+                    title={this.props.title}
+                    project={this.props.project}
+                />
+            );
+        } else {
+            return (
+                <Timer
+                    title={this.props.title}
+                    project={this.props.project}
+                    elapsed={this.props.elapsed}
+                    runningSince={this.props.runningSince}
+                />
+            );
+        }
+    },
+});
